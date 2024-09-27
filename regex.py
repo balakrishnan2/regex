f400e649e207fec2ab57aab8f4b5a1f739d9362e
Balakrishnan <balakrishnan.meenakshisundaram@eleviant.com>
2024-09-27 14:04:24 +0530

    Added few more function

--- regex.py
+++ regex.py
@@ -1,6 +1,47 @@
 import re
 
 
-def wordsearch(sentence, search_word):
+def word_search(sentence, search_word):
+    """
+    Function to find given word in sentence or not
+    Parameter: sentence(string), search_word(string)
+    return: True / false (boolean)
+    """
     return True if re.search(search_word, sentence) else False
 
+def word_search_with_endspan(sentence, search_word, endspan=None):
+    """
+    Function to find given word in sentence or not
+    Parameter: sentence(string), search_word(string)
+    return: search word with given span / only return word
+    """ 
+    if endspan:
+        return re.search(search_word+"\w"*endspan, sentence)
+    else:
+        return re.search(search_word, sentence)
+
+def remove_duplicates_letter(word):
+    """
+    Function to remove duplicates
+    Parameter: word (string)
+    return: string
+    """
+    return re.sub(r"(.)(?=\1+)", "", word)
+
+def separate_numbers_alphabets(str):
+    """
+    Function to separate the numbers and alphabets
+    Parameter: string
+    return : numbers and alphabets (string)
+    """
+    numbers = re.findall(r'[0-9]+', str)
+    alphabets = re.findall(r'[a-zA-Z]+', str)
+    return *numbers, *alphabets 
+
+def count_word_occurance(sentence, str):
+    """
+    Function to count occurance of the given word in the sentence
+    Parameter : sentence, word
+    retunr: count (number)
+    """
+    return len(re.findall(str, sentence))
